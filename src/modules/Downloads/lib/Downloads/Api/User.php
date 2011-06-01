3068a6663beddc27fc3b6a855367d42fda8b05f7
Craig Heydenburg <craigh@mac.com>
2011-06-01 19:36:17 -0400

    remove user links

--- src/modules/Downloads/lib/Downloads/Api/User.php
+++ src/modules/Downloads/lib/Downloads/Api/User.php
@@ -13,33 +13,5 @@
  */
 class Downloads_Api_User extends Zikula_AbstractApi
 {
-    /**
-     * Get available user links
-     *
-     * @return array array of admin links
-     */
-    public function getlinks()
-    {
-        // Define an empty array to hold the list of admin links
-        $links = array();
-        
-        if (SecurityUtil::checkPermission('Downloads::', '::', ACCESS_ADMIN)) {
-            $links[] = array(
-                'url' => ModUtil::url('Downloads', 'admin', 'modifyconfig'),
-                'text' => $this->__('Admin'),
-                'class' => 'z-icon-es-config');
-        }
-        
-        $links[] = array(
-            'url' => ModUtil::url('Downloads', 'user', 'view'),
-            'text' => $this->__('Download List'),
-            'class' => 'z-icon-es-list');
 
-        $links[] = array(
-            'url' => ModUtil::url('Downloads', 'user', 'edit'),
-            'text' => $this->__('New download'),
-            'class' => 'z-icon-es-new');
-                            
-        return $links;
-    }
 } // end class def
