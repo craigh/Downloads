f8aeffddf3bd8df3e77181b8d6d91a5ea8e1f10a
Craig Heydenburg <craigh@mac.com>
2011-06-01 22:17:15 -0400

    remove upload folder on unintall and change default location

--- src/modules/Downloads/lib/Downloads/Installer.php
+++ src/modules/Downloads/lib/Downloads/Installer.php
@@ -88,9 +88,9 @@ class Downloads_Installer extends Zikula_AbstractInstaller
         // clean up associated categories
         DBUtil::deleteWhere('categories_mapobj', "cmo_modname='Downloads'");
         
-        //remove files from data folder?
-        // remove Doctrine entity folder in temp?
-        // /ztemp/doctrinemodels/GeneratedDoctrineModel/Downloads/
+        //remove files from data folder
+        $uploaddir = DataUtil::formatForOS($this->getVar('upload_folder'));
+        FileUtil::deldir($uploaddir, true);
         
         // remove all module vars
         $this->delVars();
@@ -104,7 +104,6 @@ class Downloads_Installer extends Zikula_AbstractInstaller
     private function createUploadDir()
     {
         // upload dir creation
-        // $uploaddir = FileUtil::getDataDirectory().'/Downloads/files';
         $uploaddir = $this->getVar('upload_folder');
 
         if (mkdir($uploaddir, System::getVar('system.chmod_dir', 0777), true)) {
--- src/modules/Downloads/lib/Downloads/Util.php
+++ src/modules/Downloads/lib/Downloads/Util.php
@@ -51,7 +51,7 @@ class Downloads_Util
             'cclause' => 'ASC',
             'popular' => 10,
             'torrent' => false,
-            'upload_folder' => FileUtil::getDataDirectory().'/Downloads/files',
+            'upload_folder' => FileUtil::getDataDirectory().'/Downloads',
             'screenshot_folder' => '',
             'cache_folder' => '',
             'treeview' => false,
