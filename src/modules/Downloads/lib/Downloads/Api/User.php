6f5c0b30b4ff5ce54bfa360c5a4b775acf7360ed
Craig Heydenburg <craigh@mac.com>
2011-06-07 21:53:46 -0400

    refactor api to count results from query

--- src/modules/Downloads/lib/Downloads/Api/User.php
+++ src/modules/Downloads/lib/Downloads/Api/User.php
@@ -19,15 +19,17 @@ class Downloads_Api_User extends Zikula_AbstractApi
         $startnum = isset($args['startnum']) ? $args['startnum'] : 0;
         $orderby = isset($args['orderby']) ? $args['orderby'] : 'title';
         $orderdir = isset($args['orderdir']) ? $args['orderdir'] : 'ASC';
-        $limit = $this->getVar('perpage');
+        $limit = isset($args['limit']) ? $args['limit'] : $this->getVar('perpage');
 
         $tbl = Doctrine_Core::getTable('Downloads_Model_Download');
         $q = $tbl->createQuery('d')
                 ->expireQueryCache()
                 ->expireResultCache()
                 ->orderBy("$orderby $orderdir")
-                ->offset($startnum)
-                ->limit($limit);
+                ->offset($startnum);
+        if ($limit > 0) {
+            $q->limit($limit);
+        }
         if (!empty($category)) {
             $q->where("d.cid = ?", $category);
         }
@@ -44,21 +46,8 @@ class Downloads_Api_User extends Zikula_AbstractApi
 
     public function countQuery($args)
     {
-        $category = isset($args['category']) ? $args['category'] : 0;
-        $q = Doctrine_Core::getTable('Downloads_Model_Download')
-                ->createQuery()
-                ->expireQueryCache()
-                ->expireResultCache();
-        if (!empty($category)) {
-            $q->where("cid=$category");
-        }
-        $items = $q->execute();
-        foreach ($items as $key => $item) {
-            if ((!SecurityUtil::checkPermission('Downloads::Item', $item['lid'] . '::', ACCESS_READ)) ||
-                (!SecurityUtil::checkPermission('Downloads::Category', $item['cid'] . '::', ACCESS_READ))) {
-                unset($items[$key]);
-            }
-        }
+        $args['limit'] = -1;
+        $items = $this->getall($args);
         return count($items);
     }
 
