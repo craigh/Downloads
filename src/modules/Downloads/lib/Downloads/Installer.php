3d7b8591ca869b30bfd19b1c084834a0ed74e753
Craig Heydenburg <craigh@mac.com>
2012-04-14 08:44:55 -0400

    bump version

--- src/modules/Downloads/lib/Downloads/Installer.php
+++ src/modules/Downloads/lib/Downloads/Installer.php
@@ -136,6 +136,8 @@ CHANGE `pn_description` `description` VARCHAR( 254 ) CHARACTER SET utf8 COLLATE 
                 HookUtil::registerSubscriberBundles($this->version->getHookSubscriberBundles());
 
             case '3.0.0':
+                // no changes
+            case '3.1.0':
             //future development
         }
 
--- src/modules/Downloads/lib/Downloads/Version.php
+++ src/modules/Downloads/lib/Downloads/Version.php
@@ -18,7 +18,7 @@ class Downloads_Version extends Zikula_AbstractVersion
         $meta['displayname'] = $this->__('Downloads');
         $meta['url'] = $this->__(/* !used in URL - nospaces, no special chars, lcase */'downloads');
         $meta['description'] = $this->__('Zikula file download manager');
-        $meta['version'] = '3.0.0';
+        $meta['version'] = '3.1.0';
 
         $meta['securityschema'] = array('Downloads::' => '::',
             'Downloads::Category' => 'CategoryID::',
